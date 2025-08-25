629a575e11e0970c42e88e83d2d18fbf863f041a
Imanol Peña <jdbughanuel758@gmail.com>
2025-08-24 22:39:56 -0500

    Modificar luego

--- app.js
+++ app.js
@@ -6,3 +6,34 @@ lo agregarán a una lista visible al hacer clic en "Adicionar".
 -Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
 -Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la
 lista y se mostrará en la página. */
+
+let listaDeAmigos = []; //Lista inicial vacía
+let nombre = '';
+
+function asignarElementos(elemento,texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo(){
+    input = document.getElementById("amigo");
+    nombre = input.value.trim();
+    if (nombre == ''){
+        alert('Inserte un nombre válido, por favor');
+        return;
+    }
+    listaDeAmigos.push(nombre);
+    mostrarLista();
+}
+
+function mostrarLista(){
+    lista = document.getElementById("listaAmigos");
+    lista.innerHTML=""; //Se limpia la lista
+
+    listaDeAmigos.forEach(amigo,index){
+        li = document.createElement("li");
+        li.textContent = amigo;
+        
+    }
+}
